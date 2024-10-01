5650819e6118a9a5f42fc65151921630aa0668aa
Radwa-Adell <elshorbagyradwa81@gmail.com>
2024-09-30 22:31:47 -0700

    Finish Project

--- FlowerStore.java
+++ FlowerStore.java
@@ -10,30 +10,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
-/**
- *
- * @author DELL
- */
 public class FlowerStore extends Application {
-   
-    
-    
+  
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
        
-       
-        
+     
         stage.setScene(scene);
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
